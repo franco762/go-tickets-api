7ad795568cfcdc915a6771af45926532c95fa75e
Fabian <fabianfranco2001@gmail.com>
2021-07-22 20:00:26 -0500

    Structuring application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+// App : la estructura que contiene
+// punteros a conexiones de base de datos
+type App struct{}
+
+// Run - Controla el inicio de la aplicación
+func (app *App) Run() error {
+	fmt.Println("Configurando aplicación")
+	return nil
+}
+
 func main() {
 	fmt.Println("GO TICKETS API")
+
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Errror al iniciar la API")
+		fmt.Println(err)
+	}
 }
